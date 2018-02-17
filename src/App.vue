efbf99cd543237f47826304aada1a9e4dbb7065f
ashvin777 <ashvin.suthar777@gmail.com>
2018-02-18 00:39:55 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+html, body{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/pages/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    
+  </div>
+</template>
+
+<script src='./main.js'>
+</script>
+
+<style scoped lang="scss">
+.page{
+  height: 100%;
+  width: 100%;
+}
+</style>
